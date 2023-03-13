61ae1d5f7d0d5e12962e64c7b571788c8cbb85c8
VolchkovIlia <113889753+VolchkovIlia@users.noreply.github.com>
2023-03-13 10:49:19 +0300

    Add files via upload

---
+++ Lab 10/Lab 10/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Lab_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtArray = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtArray
+            // 
+            this.txtArray.Location = new System.Drawing.Point(12, 3);
+            this.txtArray.Multiline = true;
+            this.txtArray.Name = "txtArray";
+            this.txtArray.Size = new System.Drawing.Size(340, 174);
+            this.txtArray.TabIndex = 0;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(425, 191);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 1;
+            this.btnCalc.Text = "Вычислить";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 236);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtArray);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtArray;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ Lab 10/Lab 10/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int[] arr=new int[15];
+            int sum = 0;
+            int max = arr[0];
+            int min = arr[0];
+            Random rand= new Random();
+            txtArray.Text = "Массив: ";
+            for (int i=0;i<arr.Length;i++)
+            {
+                arr[i]= rand.Next(20);
+                sum += arr[i];
+                txtArray.Text += arr[i].ToString() + " ";
+                if (arr[i]>max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i]<min)
+                {
+                    min = arr[i];
+                }
+                
+            }
+            Array.Sort(arr);
+            txtArray.Text += "\r\nSort: ";
+          for (int i=0;i<arr.Length;i++)
+          {
+                txtArray.Text +=arr[i].ToString() + " ";
+          }
+            txtArray.Text += "\r\nMax: " + max;
+            txtArray.Text += "\r\nMin: " + min;
+            txtArray.Text += "\r\nСумма: "+sum.ToString();
+            double average = (double)sum / arr.Length;
+            txtArray.Text += "\r\nСреднее арифметическое: " + average.ToString();
+
+        }
+    }
+}
